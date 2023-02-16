416db707e1f031ae026902148f08a5d79aa09246
khyan22 <khyanalvarez@gmail.com>
2023-02-15 22:46:12 -0500

    init commit

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Nav() {
+  return(
+    <nav>
+      <ul>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li>
+          <a href='#resume'>Resume</a>
+        </li>
+        <li>
+          <a href='#Contact'>Contact Me</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
